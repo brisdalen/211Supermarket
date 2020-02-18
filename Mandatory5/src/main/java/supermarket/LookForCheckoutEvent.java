f29cf0c723ff7726749ee8105a8fda487e442685
Bjørnar W. Risdalen <bwendelborgrisdalen@gmail.com>
2020-02-18 12:12:12 +0100

    Started looping

--- Mandatory5/src/main/java/supermarket/LookForCheckoutEvent.java
+++ Mandatory5/src/main/java/supermarket/LookForCheckoutEvent.java
@@ -15,16 +15,29 @@ public class LookForCheckoutEvent extends Event {
     @Override
     public Event happen() {
 
+        Checkout[] checkouts = customer.shop.getCheckouts();
+
         // Hvis det skjer noe galt returneres en error event
         Event event = ErrorEvent.getInstance(getTimeAfterLooking4Q());
 
-        for(Checkout c : customer.shop.getCheckouts()) {
-            if(c.customers.size() > 0) {
-                event = new JoinQEvent(getTimeAfterLooking4Q(), c, customer);
-            } else {
+        int i = 0;
+        int smallesQIndex = i;
+        int smallesQLength = checkouts[i].customers.size();
+
+        for(Checkout c : checkouts) {
+
+            if(c.customers.size() == 0) {
                 // hvis ingen kø, gå direkte til å betale
                 event = new CheckoutEvent(getTimeAfterLooking4Q(), c, customer);
+                break;
             }
+
+            if(c.customers.size() < smallesQLength) {
+                smallesQLength = c.customers.size();
+                smallesQIndex = i;
+            }
+
+            i++;
         }
 
         return event;
