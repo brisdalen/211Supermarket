0f6ba35c8b8ef100ef63de37dccda96db068c7de
Bjørnar W. Risdalen <bwendelborgrisdalen@gmail.com>
2020-02-28 09:39:17 +0100

    small time change

--- Mandatory5/src/main/java/supermarket/EndShoppingEvent.java
+++ Mandatory5/src/main/java/supermarket/EndShoppingEvent.java
@@ -18,16 +18,18 @@ import eventsim.EventSim;
  */
 public class EndShoppingEvent extends Event {
     Customer customer;
+    int leaveTime;
 
     public EndShoppingEvent(int time, Customer customer) {
         super(time);
         this.customer = customer;
+        this.leaveTime = time;
     }
 
     @Override
     public Event happen() {
         System.out.println(Constants.ANSI_BRIGHT_BLUE + "customer leaving @ time: " + EventSim.getClock());
-        customer.leaveTime = customer.checkoutTime + customer.checkoutDuration;
+        customer.leaveTime = leaveTime;
         return null;
     }
 
